2173e5924cb6ff35118207af846e472e68818560
Jonald <20171948@s.ubaguio.edu>
2026-03-12 12:36:52 +0800

    Changes in view-details component

--- src/app/view-details/view-details.component.ts
+++ src/app/view-details/view-details.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 selector:'app-view-details',
 templateUrl:'./view-details.component.html'
 })
 
-export class ViewDetailsComponent{}
+export class ViewDetailsComponent implements OnInit{
+  price: number = 0;
+  description: string = "";
+  category: string = "";
+  stock: number = 0;
+  status: string = "";
+  brand: string = "";
+  rating: number = 0;
+  supplier: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}

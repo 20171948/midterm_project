a65c2058e903164cf690650173d777a9862baea9
Jonald <20171948@s.ubaguio.edu>
2026-03-12 11:18:30 +0800

    Changes the information in all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; 
-import { ProductsComponent } from './products/products.component';  
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,  
-  imports: [RouterModule, ProductsComponent], 
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'midtermProject';
+title(title: any) {
+  throw new Error('Method not implemented.');
+}
+
+studentName: string = "Jonald Alea";
+score: number = 90;
+
+imageUrl: string = "https://angular.io/assets/images/logos/angular/angular.png";
+
+isDisabled: boolean = false;
+
+message: string = "";
+
+showMessage(){
+this.message = "Button Clicked Successfully!";
+}
+
 }
